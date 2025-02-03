577f1adc16f51068c5986f0dceb0c9b0b895ceee
cansu <cansualtunsoy@gmail.com>
2025-02-03 23:15:16 +0300

    filePath

---
+++ src/test/java/C06_actionsClass_fileTestleri/C06_dosyaYolunuDinamikYapma.java
@@ -0,0 +1,59 @@
+package C06_actionsClass_fileTestleri;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C06_dosyaYolunuDinamikYapma {
+
+    @Test
+    public void test01() {
+
+
+        String downloadDeneme = "\\Users\\Cansu\\Downloads\\deneme.txt";
+
+        Assertions.assertTrue(Files.exists(Paths.get(downloadDeneme)));
+
+
+        String dinamikAnaDosyaYolu = System.getProperty("user.home");
+        System.out.println(dinamikAnaDosyaYolu);
+
+
+        /*
+        Eger dosya yolunu normal olarak yazarsak  \Users\Cansu\Downloads\deneme.txt sadece dosya yolunu olusturan kişinin
+        pc de calışır, başka pc de calışmaz, Java ortak calisabilmemiz icin
+         her pc de farkli olan bastaki kismi alabilecegimiz bir kod hazirlamistir
+
+         Herkeste farkli olan kisi: \Users\Cansu
+        Herkeste ortak olan kisim : \Downloads\deneme.txt
+
+         */
+
+
+        String dinamikDownloads = System.getProperty("user.home") + "/Downloads/deneme.txt";
+
+        Assertions.assertTrue(Files.exists(Paths.get(dinamikDownloads)));
+
+
+        String downloadsDeneme = "\\Users\\Cansu\\Downloads\\deneme.txt";
+
+        Assertions.assertTrue(Files.exists(Paths.get(downloadsDeneme)));
+
+
+        //  Herkeste farkli olan kisim:  C:\Users\Cansu\IdeaProjects\Team_JUnit
+        //  Herkeste ayni olan kisim :  \src\test\java\day09\deneme.txt==> Abslout degeri
+
+        System.out.println(System.getProperty("user.dir"));
+
+        String dinamikPojeDosyaYolu = System.getProperty("user.dir");
+
+        dinamikAnaDosyaYolu = dinamikPojeDosyaYolu + "\\src\\test\\java\\day09_actionsClass_fileTestleri\\deneme.txt";
+
+
+        Assertions.assertTrue(Files.exists(Paths.get(dinamikAnaDosyaYolu)));
+
+
+    }
+}
---
+++ src/test/java/C06_actionsClass_fileTestleri/C07_dosyaIndirmeTest.java
@@ -0,0 +1,34 @@
+package C06_actionsClass_fileTestleri;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import utilities.TestBaseEach;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C07_dosyaIndirmeTest extends TestBaseEach {
+
+
+    @Test
+    public void test01() {
+
+
+        // 1. https://the-internet.herokuapp.com/download adresine gidelim.
+        driver.get("https://the-internet.herokuapp.com/download");
+
+
+        //2. logo.png dosyasını indirelim
+        driver.findElement(By.xpath("//*[.='download.jpeg']")).click();
+
+        //3. Dosyanın başarıyla indirilip indirilmediğini test edelim
+
+        String dinamikDosyaYolu = System.getProperty("user.home") + "\\Cansu\\Downloads\\85378a82-f7f5-4f2c-a49b-f642a9bbf161.tmp";
+
+        Assertions.assertTrue(Files.exists(Paths.get(dinamikDosyaYolu)));
+
+
+    }
+
+}
